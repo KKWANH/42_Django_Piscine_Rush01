777da90eca7069f5291dbb08b0116e118f83000b
kosumin <be_0302@naver.com>
2021-08-10 20:44:31 +0900

    nginx

--- Rush01_Site/Rush01/settings.py
+++ Rush01_Site/Rush01/settings.py
@@ -2,8 +2,8 @@ import os
 from pathlib import Path
 
 SECRET_KEY = 'django-insecure-3a^-4@0)wq_*333$s8+yqd@zxtpcug1#0og5mtdrw*1(sdskbi'
-DEBUG = True
-ALLOWED_HOSTS = ['localhost', 'web']
+DEBUG = False
+ALLOWED_HOSTS = ['web']
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 INSTALLED_APPS = [
